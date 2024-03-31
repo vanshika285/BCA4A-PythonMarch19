c5581c4d9b7d645746a70c8f299fe7f6d77efd49
vanshika285 <156657046+vanshika285@users.noreply.github.com>
2024-03-31 16:13:51 +0530

    Add files via upload

---
+++ 4.1.python.py
@@ -0,0 +1,12 @@
+def main():
+    count = 1
+    max_count = 5
+
+    print("Printing a text using a while loop:")
+
+    while count <= max_count:
+        print("This is line", count)
+        count += 1
+
+if __name__ == "__main__":
+    main()
